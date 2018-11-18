edf2901661cf5dc8975d39b6dd95640c38f21ed4
yastaheran <y.taheranpour@gmail.com>
2018-11-18 14:38:40 +0100

    Create Split.java

---
+++ Ukesoppgaver/2/Split.java
@@ -0,0 +1,10 @@
+class Split {
+	public final Key key;
+	public final Node left;
+	public final Node right;
+
+	public Split(Key k, Node l, Node r) {
+	    key = k; left = l; right = r;
+	}
+    }
+}
