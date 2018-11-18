e86f34a8c5dfd937e20d9d6cbb45098bc647f07f
yastaheran <y.taheranpour@gmail.com>
2018-11-18 14:49:31 +0100

    Create HeapInterface.java

---
+++ Ukesoppgaver/HeapInterface.java
@@ -0,0 +1,7 @@
+/** Exercise 5 Implement the interface below with a BinaryHeap. 4.5 BinaryHeap with interface HeapInterface<T> */
+
+interface HeapInterface<T>{
+    public void insert(int pri, T o);
+    public T deleteMin();
+}
+  
